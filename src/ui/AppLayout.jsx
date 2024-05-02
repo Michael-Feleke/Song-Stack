cec76caf256e54b0dde182057be559f8768ba9af
Michael Feleke <felekemichael999@gmail.com>
2024-05-03 01:02:07 +0300

    refactor: Update AppLayout component to include Sidebar, Header, and Main

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,11 +1,29 @@
 import { Outlet } from "react-router-dom";
+import Sidebar from "./Sidebar";
+import Header from "./Header";
+import styled from "@emotion/styled";
+
+const StyledAppLayout = styled.div`
+  display: grid;
+  grid-template-columns: 26rem 1fr;
+  grid-template-rows: auto 1fr;
+  height: 100vh;
+`;
+
+const Main = styled.main`
+  padding: 4rem 4.8rem 6.4rem;
+  background-color: var(--color-grey-50);
+`;
 
 function AppLayout() {
   return (
-    <div>
-      <p>App layout</p>
-      <Outlet />
-    </div>
+    <StyledAppLayout>
+      <Sidebar />
+      <Header />
+      <Main>
+        <Outlet />
+      </Main>
+    </StyledAppLayout>
   );
 }
 

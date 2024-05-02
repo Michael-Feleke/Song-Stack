48db911cbbe21a9c4ef5678f09adff2889526155
Michael Feleke <felekemichael999@gmail.com>
2024-05-02 22:27:02 +0300

    including emotion and styled system to the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+import styled from "@emotion/styled";
+import { space } from "styled-system";
+
+const H1 = styled.h1`
+  ${space}
+  color: red;
+`;
+
 function App() {
-  return <div>Project Starter</div>;
+  return (
+    <H1 m={4} p={2}>
+      Project Starter
+    </H1>
+  );
 }
 
 export default App;

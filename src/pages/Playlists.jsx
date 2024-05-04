e25d7b0962c83b934fe7014b0a5320a769b0ba9c
Michael Feleke <felekemichael999@gmail.com>
2024-05-05 01:57:05 +0300

    feat: Add lazy loading for images in Favorites page

--- src/pages/Playlists.jsx
+++ src/pages/Playlists.jsx
@@ -1,5 +1,20 @@
+import { useState } from "react";
+import CreatePlaylistForm from "../features/playlist/CreatePlaylistForm";
+import Button from "../ui/Button";
+import Modal from "../ui/Modal";
+
 function Playlists() {
-  return <div>Playlists</div>;
+  const [showModal, setShowModal] = useState(false);
+  const toggleModal = () => setShowModal((prev) => !prev);
+
+  return (
+    <>
+      <Button onClick={toggleModal}>Add Playlist</Button>
+      <Modal isOpen={showModal} onClose={toggleModal}>
+        <CreatePlaylistForm toggleModal={toggleModal} showModal={showModal} />
+      </Modal>
+    </>
+  );
 }
 
 export default Playlists;

4853d592a25963438d591139cd178d725693ff7d
Michael Feleke <felekemichael999@gmail.com>
2024-05-04 07:06:10 +0300

    feat: Add postSong action and saga for creating new songs

--- src/features/Song/songSaga.js
+++ src/features/Song/songSaga.js
@@ -1,6 +1,14 @@
 import { takeEvery, put, call, all, fork } from "redux-saga/effects";
-import { getSongsStart, getSongsSuccess, getSongsFailure } from "./songSlice";
-import api, { fetchSongs } from "../../api";
+import {
+  getSongsStart,
+  getSongsSuccess,
+  getSongsFailure,
+  postSongSuccess,
+  postSongFailure,
+  postSongStart,
+} from "./songSlice";
+import api, { fetchSongs, postCreatedSong } from "../../api";
+import { getSongs as getSongsFn } from "./songSlice";
 
 function* getSongs() {
   try {
@@ -12,10 +20,27 @@ function* getSongs() {
   }
 }
 
-function* watchGetSongs() {
+function* postSong(action) {
+  try {
+    yield put(postSongStart());
+    const song = action.payload;
+    console.log(song);
+    yield call(postCreatedSong, song, api);
+    yield put(postSongSuccess());
+    yield put(getSongsFn());
+  } catch (error) {
+    yield put(postSongFailure(error.message));
+  }
+}
+
+function* watchPostSong() {
+  yield takeEvery("songs/postSong", postSong);
+}
+
+function* watchGetSong() {
   yield takeEvery("songs/getSongs", getSongs);
 }
 
 export function* songsSaga() {
-  yield all([fork(watchGetSongs)]);
+  yield all([fork(watchGetSong), fork(watchPostSong)]);
 }

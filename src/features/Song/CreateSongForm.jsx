f602d56aa51c42bc8cd920f7ab74b165103f235d
Michael Feleke <felekemichael999@gmail.com>
2024-05-04 13:53:13 +0300

    refactor: Add Spinnermini component and loading state to CreateSongForm component

--- src/features/Song/CreateSongForm.jsx
+++ src/features/Song/CreateSongForm.jsx
@@ -2,8 +2,10 @@ import styled from "@emotion/styled";
 import { useState } from "react";
 import Button from "../../ui/Button";
 import { HiMusicalNote } from "react-icons/hi2";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { postSong, updateSong } from "./songSlice";
+import Spinnermini from "../../ui/Spinnermini";
+import { HiXCircle } from "react-icons/hi";
 
 const FormContainer = styled.form`
   display: flex;
@@ -43,7 +45,19 @@ const StyledButton = styled(Button)`
   margin-top: 1.5rem;
 `;
 
-function CreateSongForm({ song = null, isEditing = false }) {
+const CloseContainer = styled.div`
+  display: flex;
+  justify-content: end;
+  margin-bottom: 1rem;
+  cursor: pointer;
+`;
+
+function CreateSongForm({
+  song = null,
+  isEditing = false,
+  toggleModal,
+  showModal,
+}) {
   const id = isEditing ? song.id : null;
   const [title, setTitle] = useState(isEditing ? song.title : "");
   const [artist, setArtist] = useState(isEditing ? song.artist : "");
@@ -53,6 +67,8 @@ function CreateSongForm({ song = null, isEditing = false }) {
   const [releasedDate, setReleasedDate] = useState(
     isEditing ? song.releasedDate : ""
   );
+
+  const status = useSelector((state) => state.songs.status);
 
   const dispatch = useDispatch();
 
@@ -65,75 +81,89 @@ function CreateSongForm({ song = null, isEditing = false }) {
       genere: genere == "" ? "UNKNOWN" : genere,
       album: album == "" ? "UNKNOWN" : album,
       composer: composer == "" ? "UNKNOWN" : composer,
-      releasedDate: releasedDate == "UNKNOWN" ? "" : releasedDate,
+      releasedDate: releasedDate == "" ? "UNKNOWN" : releasedDate,
       image: "logo-light.png",
     };
 
     isEditing ? dispatch(updateSong(song)) : dispatch(postSong(song));
   };
 
-  return (
-    <FormContainer>
-      <InputContainer>
-        <InputLabel>Title</InputLabel>
-        <InputField
-          type="text"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-          placeholder="Enter title"
-          required
-        />
-      </InputContainer>
-      <InputContainer>
-        <InputLabel>Artist</InputLabel>
-        <InputField
-          type="text"
-          value={artist}
-          onChange={(e) => setArtist(e.target.value)}
-          placeholder="Enter artist"
-        />
-      </InputContainer>
-      <InputContainer>
-        <InputLabel>Genre</InputLabel>
-        <InputField
-          type="text"
-          value={genere}
-          onChange={(e) => setGenere(e.target.value)}
-          placeholder="Enter genre"
-        />
-      </InputContainer>
-      <InputContainer>
-        <InputLabel>Album</InputLabel>
-        <InputField
-          type="text"
-          value={album}
-          onChange={(e) => setAlbum(e.target.value)}
-          placeholder="Enter album"
-        />
-      </InputContainer>
-      <InputContainer>
-        <InputLabel>Composer</InputLabel>
-        <InputField
-          type="text"
-          value={composer}
-          onChange={(e) => setComposer(e.target.value)}
-          placeholder="Enter composer"
-        />
-      </InputContainer>
-      <InputContainer>
-        <InputLabel>Released Date</InputLabel>
-        <InputField
-          type="date"
-          value={releasedDate}
-          onChange={(e) => setReleasedDate(e.target.value)}
-          placeholder="Enter released date"
-        />
-      </InputContainer>
+  const handleClose = () => {
+    toggleModal((showModal) => !showModal);
+  };
 
-      <StyledButton onClick={handleSubmit}>
-        <HiMusicalNote /> {isEditing ? "Edit" : "Create"} Song
-      </StyledButton>
-    </FormContainer>
+  return (
+    <>
+      <CloseContainer>
+        <HiXCircle onClick={handleClose} size={30} />
+      </CloseContainer>
+      <FormContainer>
+        <InputContainer>
+          <InputLabel>Title</InputLabel>
+          <InputField
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            placeholder="Enter title"
+            required
+          />
+        </InputContainer>
+        <InputContainer>
+          <InputLabel>Artist</InputLabel>
+          <InputField
+            type="text"
+            value={artist}
+            onChange={(e) => setArtist(e.target.value)}
+            placeholder="Enter artist"
+          />
+        </InputContainer>
+        <InputContainer>
+          <InputLabel>Genre</InputLabel>
+          <InputField
+            type="text"
+            value={genere}
+            onChange={(e) => setGenere(e.target.value)}
+            placeholder="Enter genre"
+          />
+        </InputContainer>
+        <InputContainer>
+          <InputLabel>Album</InputLabel>
+          <InputField
+            type="text"
+            value={album}
+            onChange={(e) => setAlbum(e.target.value)}
+            placeholder="Enter album"
+          />
+        </InputContainer>
+        <InputContainer>
+          <InputLabel>Composer</InputLabel>
+          <InputField
+            type="text"
+            value={composer}
+            onChange={(e) => setComposer(e.target.value)}
+            placeholder="Enter composer"
+          />
+        </InputContainer>
+        <InputContainer>
+          <InputLabel>Released Date</InputLabel>
+          <InputField
+            type="date"
+            value={releasedDate}
+            onChange={(e) => setReleasedDate(e.target.value)}
+            placeholder="Enter released date"
+          />
+        </InputContainer>
+        {status === "loading" ? (
+          <StyledButton disabled style={{ opacity: "0.5" }}>
+            <Spinnermini /> {isEditing ? "Updating" : "Creating"}
+          </StyledButton>
+        ) : (
+          <StyledButton onClick={handleSubmit}>
+            <HiMusicalNote /> {isEditing ? "Edit" : "Create"} Song
+          </StyledButton>
+        )}
+      </FormContainer>
+    </>
   );
 }
 

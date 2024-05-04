b012671698d3d4e6b0a80683012f524f1dcd1ed3
Michael Feleke <felekemichael999@gmail.com>
2024-05-04 23:12:57 +0300

    refactor: Update Spinner component styling and structure

--- src/ui/Spinner.jsx
+++ src/ui/Spinner.jsx
@@ -1,23 +1,48 @@
 import styled from "@emotion/styled";
-import { keyframes } from "@emotion/react";
 
-const rotate = keyframes`
-  to {
-    transform: rotate(1turn)
+const SpinnerContainer = styled.div`
+  margin: 4rem auto;
+  width: 6rem;
+`;
+
+const SpinnerDiv = styled.div`
+  width: 6rem;
+  height: 6rem;
+  border-radius: 9999px;
+  border-width: 8px;
+  border-style: dashed;
+  border-color: rgb(34, 197, 94);
+  border-top-color: transparent;
+  animation: spin 1s linear infinite;
+  @keyframes spin {
+    to {
+      transform: rotate(360deg);
+    }
   }
 `;
 
-const Spinner = styled.div`
-  margin: 4.8rem auto;
-
-  width: 6.4rem;
-  aspect-ratio: 1;
-  border-radius: 50%;
-  background: radial-gradient(farthest-side, var(--color-brand-600) 94%, #0000)
-      top/10px 10px no-repeat,
-    conic-gradient(#0000 30%, var(--color-brand-600));
-  -webkit-mask: radial-gradient(farthest-side, #0000 calc(100% - 10px), #000 0);
-  animation: ${rotate} 1.5s infinite linear;
+const SpinnerPage = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  width: 100vw;
+  background-color: rgba(0, 0, 0, 0.1);
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
 `;
+
+function Spinner() {
+  return (
+    <SpinnerPage>
+      <SpinnerContainer>
+        <SpinnerDiv></SpinnerDiv>
+      </SpinnerContainer>
+    </SpinnerPage>
+  );
+}
 
 export default Spinner;

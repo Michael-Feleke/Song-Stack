d7a52281b353b17270c20c0efc44b0a322bcfd28
Michael Feleke <felekemichael999@gmail.com>
2024-05-05 22:38:09 +0300

    refactor: Update Heading component styles for different heading levels

--- src/ui/Heading.jsx
+++ src/ui/Heading.jsx
@@ -5,26 +5,36 @@ import { space, typography } from "styled-system";
 const Heading = styled.h1`
   ${space}
   ${typography}
-  color: var(--color-brand-600);
   ${(props) =>
     props.as === "h1" &&
-    css`
-      font-size: 3.6rem;
-      font-weight: 700;
-    `}
-  ${(props) =>
-    props.as === "h2" &&
     css`
       font-size: 3rem;
       font-weight: 600;
     `}
 
+  ${(props) =>
+    props.as === "h2" &&
+    css`
+      font-size: 2rem;
+      font-weight: 600;
+    `}
+    
     ${(props) =>
     props.as === "h3" &&
     css`
-      font-size: 2.4rem;
+      font-size: 2rem;
       font-weight: 500;
     `}
+    
+    ${(props) =>
+    props.as === "h4" &&
+    css`
+      font-size: 3rem;
+      font-weight: 600;
+      text-align: center;
+    `}
+    
+  line-height: 1.4;
 `;
 
 export default Heading;

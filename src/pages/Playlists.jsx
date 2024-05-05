5c83c3fa8c08f7ac58edcbc6931fc0e752c05880
Michael Feleke <felekemichael999@gmail.com>
2024-05-05 22:24:44 +0300

    feat: Add lazy loading for images in Playlists component

--- src/pages/Playlists.jsx
+++ src/pages/Playlists.jsx
@@ -4,19 +4,31 @@ import Button from "../ui/Button";
 import Modal from "../ui/Modal";
 import Row from "../ui/Row";
 import PlaylistContainer from "../features/playlist/PlaylistContainer";
+import { useSelector } from "react-redux";
+import PlaylistSongs from "./PlaylistSongs";
 
 function Playlists() {
   const [showModal, setShowModal] = useState(false);
+  const [showSongs, setShowSongs] = useState(false);
+  const [playlistId, setPlaylistId] = useState(-1);
   const toggleModal = () => setShowModal((prev) => !prev);
 
-  return (
+  const { playlists } = useSelector((state) => state.songs);
+
+  return showSongs ? (
+    <PlaylistSongs id={playlistId} />
+  ) : (
     <>
       <Button onClick={toggleModal}>Add Playlist</Button>
       <Modal isOpen={showModal} onClose={toggleModal}>
         <CreatePlaylistForm toggleModal={toggleModal} showModal={showModal} />
       </Modal>
       <Row>
-        <PlaylistContainer />
+        <PlaylistContainer
+          playlists={playlists}
+          setShowSongs={setShowSongs}
+          setPlaylistId={setPlaylistId}
+        />
       </Row>
     </>
   );

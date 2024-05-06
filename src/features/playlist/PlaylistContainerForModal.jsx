cb1ab8802b585df8a7495015cb4aa42c2e16cbf5
Michael Feleke <felekemichael999@gmail.com>
2024-05-06 08:55:03 +0300

    refactor: Update PlaylistContainerForModal component to improve layout and add create playlist functionality

--- src/features/playlist/PlaylistContainerForModal.jsx
+++ src/features/playlist/PlaylistContainerForModal.jsx
@@ -1,20 +1,23 @@
 import { useDispatch, useSelector } from "react-redux";
 import PlayListFolder from "./PlayListFolder";
 import styled from "@emotion/styled";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import PlayListFolderForModal from "./PlayListFolderForModal";
 import Header from "../../ui/Header";
 import Heading from "../../ui/Heading";
-import { HiXCircle } from "react-icons/hi";
+import { HiFolderAdd, HiXCircle } from "react-icons/hi";
+import Modal from "../../ui/Modal";
+import CreatePlaylistForm from "./CreatePlaylistForm";
+import Button from "../../ui/Button";
 
 const Container = styled.div`
   display: flex;
   flex-wrap: wrap;
-  width: 600px;
-  height: 500px;
+  width: 500px;
+  height: 300px;
   overflow-y: scroll;
-  gap: 7rem;
-  position: relative;
+  gap: 5rem;
+  /* position: relative; */
   border-radius: 2rem;
   box-shadow: 0 0 1rem var(--bg-color);
   padding: 2rem 0;
@@ -25,29 +28,64 @@ const CloseContainer = styled.div`
   margin-bottom: 1rem;
   cursor: pointer;
 `;
+const ButtonContainer = styled.div`
+  text-align: center;
+  margin: 3rem 0 1rem;
+`;
 
-function PlaylistContainerForModal({ toggleModal, showModal, song }) {
+const Span = styled.span`
+  display: flex;
+  justify-content: center;
+`;
+
+function PlaylistContainerForModal({ toggleModal, song }) {
   const { playlists } = useSelector((state) => state.songs);
+  const [showModal, setShowModal] = useState(false);
+
+  const toggleModalToCreate = () => setShowModal((prev) => !prev);
+
+  const noPlaylistFound = Object.keys(playlists).length == 0;
 
   return (
     <>
       <CloseContainer>
-        <HiXCircle onClick={toggleModal} size={30} />
+        <HiXCircle onClick={toggleModal} color="red" size={30} />
       </CloseContainer>
       <Heading style={{ textAlign: "center" }} as="h3">
-        Choose from your playlist
+        {noPlaylistFound
+          ? "Start by creating new playlist"
+          : "Choose from your playlist"}
       </Heading>
-      <Container>
-        {playlists &&
-          playlists.map((playlist) => (
-            <PlayListFolderForModal
-              key={playlist.id}
-              playlist={playlist}
-              song={song}
-              toggleModal={toggleModal}
-            ></PlayListFolderForModal>
-          ))}
-      </Container>
+      {noPlaylistFound ? (
+        <>
+          <ButtonContainer>
+            <Span>
+              <Button onClick={toggleModalToCreate}>
+                <HiFolderAdd />
+                Add playlist
+              </Button>
+            </Span>
+          </ButtonContainer>
+          <Modal isOpen={showModal} onClose={toggleModalToCreate}>
+            <CreatePlaylistForm
+              toggleModal={toggleModalToCreate}
+              showModal={showModal}
+            />
+          </Modal>
+        </>
+      ) : (
+        <Container>
+          {playlists &&
+            playlists.map((playlist) => (
+              <PlayListFolderForModal
+                key={playlist.id}
+                playlist={playlist}
+                song={song}
+                toggleModal={toggleModal}
+              ></PlayListFolderForModal>
+            ))}
+        </Container>
+      )}
     </>
   );
 }

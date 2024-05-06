202817d3973e65e33440c90e6980f9a69b529892
Michael Feleke <felekemichael999@gmail.com>
2024-05-06 08:28:08 +0300

    refactor: Update Playlists page to add "Add playlist" button and heading

--- src/pages/Playlists.jsx
+++ src/pages/Playlists.jsx
@@ -6,6 +6,19 @@ import Row from "../ui/Row";
 import PlaylistContainer from "../features/playlist/PlaylistContainer";
 import { useSelector } from "react-redux";
 import PlaylistSongs from "./PlaylistSongs";
+import Heading from "../ui/Heading";
+import styled from "@emotion/styled";
+import { HiFolderAdd } from "react-icons/hi";
+
+const Span = styled.span`
+  display: flex;
+  justify-content: center;
+`;
+
+const ButtonContainer = styled.div`
+  text-align: center;
+  margin: 3rem 0 1rem;
+`;
 
 function Playlists() {
   const [showModal, setShowModal] = useState(false);
@@ -19,7 +32,19 @@ function Playlists() {
     <PlaylistSongs id={playlistId} />
   ) : (
     <>
-      <Button onClick={toggleModal}>Add Playlist</Button>
+      <Heading as="h1">
+        {Object.keys(playlists).length == 0
+          ? "No playlist yet"
+          : "Your Playlists"}
+      </Heading>
+      <ButtonContainer>
+        <Span>
+          <Button onClick={toggleModal}>
+            <HiFolderAdd />
+            Add playlist
+          </Button>
+        </Span>
+      </ButtonContainer>
       <Modal isOpen={showModal} onClose={toggleModal}>
         <CreatePlaylistForm toggleModal={toggleModal} showModal={showModal} />
       </Modal>

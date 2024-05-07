91ad0330e9121709ea1ff6a9bfbd30d612f566aa
Michael Feleke <felekemichael999@gmail.com>
2024-05-07 09:27:37 +0300

    refactor: Improve Header component with dynamic search and user avatar

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,13 +1,32 @@
 import styled from "@emotion/styled";
+import { useLocation } from "react-router-dom";
+import HeaderMenu from "./HeaderMenu";
+import UserAvatar from "../features/authentication/UserAvatar";
+import Search from "./Search";
 
 const StyledHeader = styled.header`
+  background-color: var(--color-grey-0);
   padding: 1.2rem 4.8rem;
   border-bottom: 1px solid var(--color-grey-100);
-  background-color: var(--color-grey-0);
+  display: flex;
+  gap: 2.4rem;
+  align-items: center;
+  justify-content: flex-end;
 `;
 
 function Header() {
-  return <StyledHeader>Header</StyledHeader>;
+  const location = useLocation();
+
+  const isSongsOrFavorites =
+    location.pathname === "/songs" || location.pathname === "/favorites";
+
+  return (
+    <StyledHeader>
+      {isSongsOrFavorites && <Search />}
+      <UserAvatar />
+      <HeaderMenu />
+    </StyledHeader>
+  );
 }
 
 export default Header;

d81a60cc0cb9f01c67ac956b04228584ea7b5499
Michael Feleke <felekemichael999@gmail.com>
2024-05-07 11:10:38 +0300

    refactor: Update UserAvatar component to use profile picture from API

---
+++ src/features/authentication/UserAvatar.jsx
@@ -0,0 +1,41 @@
+import styled from "@emotion/styled";
+// import { useUser } from "./useUser";
+
+const StyledUserAvatar = styled.div`
+  display: flex;
+  gap: 1.2rem;
+  align-items: center;
+  font-weight: 500;
+  font-size: 1.4rem;
+  color: var(--color-grey-600);
+`;
+
+const Avatar = styled.img`
+  display: block;
+  width: 4rem;
+  width: 3.6rem;
+  aspect-ratio: 1;
+  object-fit: cover;
+  object-position: center;
+  border-radius: 50%;
+  outline: 2px solid var(--color-grey-100);
+`;
+
+function UserAvatar() {
+  // const { user } = useUser();
+  // const { fullName, avatar } = user.user_metadata;
+  return (
+    <StyledUserAvatar>
+      <Avatar
+        // src={avatar || "default-user.jpg"}
+        src={"profile.jpg"}
+        // alt={`Avatar of ${fullName}`}
+        alt={`Avatar of `}
+      />
+      <span>Michael</span>
+      {/* <span>{fullName}</span> */}
+    </StyledUserAvatar>
+  );
+}
+
+export default UserAvatar;

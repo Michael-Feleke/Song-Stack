b783c1cda754e8ec61a2c7aaaaada9ecec1c1f59
Michael Feleke <felekemichael999@gmail.com>
2024-05-07 11:09:47 +0300

    feat: Add DarkModeProvider component for managing dark mode state

---
+++ src/context/darkModeContext.jsx
@@ -0,0 +1,40 @@
+import { createContext, useContext, useEffect } from "react";
+
+import { useLocalStorageState } from "../hooks/useLocalStorageState";
+
+const DarkModeContext = createContext();
+
+function DarkModeProvider({ children }) {
+  const [isDarkMode, setIsDarkMode] = useLocalStorageState(false);
+
+  useEffect(() => {
+    if (isDarkMode) {
+      document.documentElement.classList.add("dark-mode");
+      document.documentElement.classList.remove("light-mode");
+    }
+    if (!isDarkMode) {
+      document.documentElement.classList.add("light-mode");
+      document.documentElement.classList.remove("dark-mode");
+    }
+  }, [isDarkMode]);
+
+  function toggleDarkMode() {
+    setIsDarkMode((curIsDarkMode) => !curIsDarkMode);
+  }
+
+  return (
+    <DarkModeContext.Provider value={{ isDarkMode, toggleDarkMode }}>
+      {children}
+    </DarkModeContext.Provider>
+  );
+}
+
+function useDarkMode() {
+  const context = useContext(DarkModeContext);
+  if (context === undefined)
+    throw new Error("DarkModeContext was used outside of DarkModeProvider");
+
+  return context;
+}
+
+export { DarkModeProvider, useDarkMode };

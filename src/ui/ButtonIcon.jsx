c0d474bc55f62d3c69bad1bacf4438cd3179e567
Michael Feleke <felekemichael999@gmail.com>
2024-05-07 11:16:10 +0300

    refactor: Add ButtonIcon component for styled button with icon

---
+++ src/ui/ButtonIcon.jsx
@@ -0,0 +1,21 @@
+import styled from "@emotion/styled";
+
+const ButtonIcon = styled.button`
+  background: none;
+  border: none;
+  padding: 0.6rem;
+  border-radius: var(--border-radius-sm);
+  transition: all 0.2s;
+
+  &:hover {
+    background-color: var(--color-grey-100);
+  }
+
+  & svg {
+    width: 2.2rem;
+    height: 2.2rem;
+    color: var(--color-brand-600);
+  }
+`;
+
+export default ButtonIcon;

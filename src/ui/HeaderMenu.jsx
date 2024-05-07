a9c2534e0d59398543685f74fc8cd8d6f5b1b31e
Michael Feleke <felekemichael999@gmail.com>
2024-05-07 09:26:18 +0300

    feat: Add HeaderMenu component for user account, dark mode toggle, and logout functionality

---
+++ src/ui/HeaderMenu.jsx
@@ -0,0 +1,33 @@
+import ButtonIcon from "./ButtonIcon";
+import { HiOutlineUser } from "react-icons/hi2";
+import Logout from "../features/authentication/Logout";
+import { useNavigate } from "react-router-dom";
+import styled from "@emotion/styled";
+import DarkModeToggle from "./DarkModeToggle";
+
+const StyledHeaderMenu = styled.ul`
+  display: flex;
+  gap: 0.4rem;
+`;
+
+function HeaderMenu() {
+  const navigate = useNavigate();
+
+  return (
+    <StyledHeaderMenu>
+      <li>
+        <ButtonIcon onClick={() => navigate("/account")}>
+          <HiOutlineUser />
+        </ButtonIcon>
+      </li>
+      <li>
+        <DarkModeToggle />
+      </li>
+      <li>
+        <Logout />
+      </li>
+    </StyledHeaderMenu>
+  );
+}
+
+export default HeaderMenu;

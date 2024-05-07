c54d7e27c01528726e308f3955bc5aabc84b0f5b
Michael Feleke <felekemichael999@gmail.com>
2024-05-07 09:24:33 +0300

    feat:adding search button to search songs by their title

---
+++ src/ui/Search.jsx
@@ -0,0 +1,78 @@
+import styled from "@emotion/styled";
+import { HiSearch } from "react-icons/hi";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getSongsByName } from "../features/Song/songSlice";
+
+const FormContainer = styled.form`
+  display: flex;
+  padding: 0 10rem 0 2rem;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const InputContainer = styled.span`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  max-width: 400px;
+`;
+
+const InputField = styled.input`
+  padding: 0.5rem;
+  border: 1px solid var(--color-brand-600);
+  border-top-right-radius: 2rem;
+  border-bottom-right-radius: 2rem;
+  transition: border-color 0.3s ease;
+  color: var(--color-grey-900);
+  background-color: var(--color-grey-50);
+  border-left: none;
+
+  &:focus {
+    outline: none;
+    border-left: none;
+  }
+`;
+
+const Div = styled.div`
+  border: 1px solid var(--color-brand-600);
+  border-top-left-radius: 2rem;
+  border-bottom-left-radius: 2rem;
+  border-right: none;
+  padding: 0.9rem;
+  background-color: var(--color-grey-50);
+  display: flex;
+`;
+
+function Search() {
+  const [search, setSearch] = useState("");
+  const { songs } = useSelector((state) => state.songs);
+  const dispatch = useDispatch();
+
+  console.log(songs);
+
+  function handleSearchInputChange(e) {
+    const value = e.target.value;
+    setSearch(value);
+    dispatch(getSongsByName(value));
+  }
+  return (
+    <FormContainer>
+      <InputContainer>
+        <Div>
+          <HiSearch style={{ margin: "0" }} />
+        </Div>
+        <InputField
+          type="text"
+          value={search}
+          onChange={handleSearchInputChange}
+          placeholder="Search by song title..."
+          required
+        />
+      </InputContainer>
+    </FormContainer>
+  );
+}
+
+export default Search;

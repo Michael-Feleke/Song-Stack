6577b8aae81191237c2da4ee91f114d5d2aac4f7
Michael Feleke <felekemichael999@gmail.com>
2024-05-07 11:15:42 +0300

    refactor: Update GlobalStyles to support light and dark mode

--- src/styles/GlobalStyles.jsx
+++ src/styles/GlobalStyles.jsx
@@ -14,17 +14,33 @@ function GlobalStyles() {
           --color-brand-800: #166f3b;
           --color-brand-900: #125a32; /* Darkest */
 
-          --color-grey-0: #fff;
-          --color-grey-50: #f9fafb;
-          --color-grey-100: #f3f4f6;
-          --color-grey-200: #e5e7eb;
-          --color-grey-300: #d1d5db;
-          --color-grey-400: #9ca3af;
-          --color-grey-500: #6b7280;
-          --color-grey-600: #4b5563;
-          --color-grey-700: #374151;
-          --color-grey-800: #1f2937;
-          --color-grey-900: #111827;
+          &.light-mode {
+            --color-grey-0: #fff;
+            --color-grey-50: #f9fafb;
+            --color-grey-100: #f3f4f6;
+            --color-grey-200: #e5e7eb;
+            --color-grey-300: #d1d5db;
+            --color-grey-400: #9ca3af;
+            --color-grey-500: #6b7280;
+            --color-grey-600: #4b5563;
+            --color-grey-700: #374151;
+            --color-grey-800: #1f2937;
+            --color-grey-900: #111827;
+          }
+
+          &.dark-mode {
+            --color-grey-0: #18212f;
+            --color-grey-50: #111827;
+            --color-grey-100: #1f2937;
+            --color-grey-200: #374151;
+            --color-grey-300: #4b5563;
+            --color-grey-400: #6b7280;
+            --color-grey-500: #9ca3af;
+            --color-grey-600: #d1d5db;
+            --color-grey-700: #e5e7eb;
+            --color-grey-800: #f3f4f6;
+            --color-grey-900: #f9fafb;
+          }
 
           --color-blue-100: #e0f2fe;
           --color-blue-700: #0369a1;

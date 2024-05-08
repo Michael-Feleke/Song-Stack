fced36c581144b776aea914e0c2c5f693bdc3574
Michael Feleke <felekemichael999@gmail.com>
2024-05-08 14:24:15 +0300

    refactor: Update Sidebar component to remove border and improve padding

--- src/ui/Sidebar.jsx
+++ src/ui/Sidebar.jsx
@@ -4,7 +4,6 @@ import MainNav from "./MainNav";
 
 const StyledSidebar = styled.aside`
   padding: 3.2rem 2.4rem;
-  /* border-right: 1px solid var(--color-grey-100); */
   background-color: var(--color-grey-0);
 
   grid-row: 1/-1;

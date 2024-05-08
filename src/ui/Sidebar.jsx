0fb3c12cc6d5eda289458554cacc18882ee0a6f3
Michael Feleke <felekemichael999@gmail.com>
2024-05-08 12:16:52 +0300

    refactor: Update Sidebar component to hide on smaller screens

--- src/ui/Sidebar.jsx
+++ src/ui/Sidebar.jsx
@@ -4,10 +4,17 @@ import MainNav from "./MainNav";
 
 const StyledSidebar = styled.aside`
   padding: 3.2rem 2.4rem;
-  border-right: 1px solid var(--color-grey-100);
+  /* border-right: 1px solid var(--color-grey-100); */
   background-color: var(--color-grey-0);
 
   grid-row: 1/-1;
+
+  @media (max-width: 800px) {
+    padding: 10rem 1.6rem;
+  }
+  @media (max-width: 680px) {
+    display: none;
+  }
 `;
 
 function Sidebar() {

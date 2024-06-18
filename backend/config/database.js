517aef02ca2ff256b939226dfa97f0572146edea
Michael Feleke <felekemichael999@gmail.com>
2024-06-18 12:38:46 +0300

    chore: setting up the database

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,10 +1,13 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const MONGO_URI = process.env.MONGO_URI;
 
 export const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(MONGO_URI);
+    await mongoose.connect(MONGO_URI);
     console.log("Database connected successfully");
   } catch (error) {
     console.log(`Error: ${error.message}`);

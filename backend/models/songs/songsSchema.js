4bc93a09ff2b66b739630b46aa7578afbd213385
Michael Feleke <felekemichael999@gmail.com>
2024-06-18 12:39:13 +0300

    feat: Add songs schema to backend API

---
+++ backend/models/songs/songsSchema.js
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+import * as Statics from "./static.js";
+
+const Schema = mongoose.Schema;
+
+const songSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    artist: {
+      type: String,
+      required: true,
+    },
+    genre: {
+      type: String,
+      required: true,
+    },
+    album: {
+      type: String,
+      required: true,
+    },
+    composer: {
+      type: String,
+      required: true,
+    },
+    releasedDate: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+songSchema.static(Statics);
+
+export default songSchema;

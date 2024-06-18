fbe451948ed89a26a16ae2d410d7262158d6d1b4
Michael Feleke <felekemichael999@gmail.com>
2024-06-18 14:20:11 +0300

    feat: Refactor songsController.js for improved code organization and readability

--- backend/controllers/songsController.js
+++ backend/controllers/songsController.js
@@ -9,11 +9,11 @@ const getSongs = async (req, res) => {
 const getSong = async (req, res) => {
   const { id } = req.params;
   if (!mongoose.Types.ObjectId.isValid(id))
-    return res.status(404).json({ error: "NO such song" });
+    return res.status(404).json({ error: "No such song" });
 
-  const searchedSong = await song.getOneSong(id);
+  const searchedSong = await song.getSongById(id);
 
-  if (!song) return res.status(404).json({ error: "No such song" });
+  if (!searchedSong) return res.status(404).json({ error: "No such song" });
   res.status(200).json(searchedSong);
 };
 
@@ -29,11 +29,37 @@ const createSong = async (req, res) => {
   };
 
   try {
-    const createdSong = await song.createOneSong(newSong);
+    const createdSong = await song.createNewSong(newSong);
     res.status(200).json(createdSong);
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
 };
 
-export { getSongs, createSong, getSong };
+const deleteSong = async (req, res) => {
+  const { id } = req.params;
+  if (!mongoose.Types.ObjectId.isValid(id))
+    return res.status(404).json({ error: "No such song" });
+
+  const deletedSong = await song.deleteSongById(id);
+
+  if (!deletedSong) return res.status(404).json({ error: "No such song" });
+  res.status(200).json(deletedSong);
+};
+
+const updateSong = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id))
+    return res.status(404).json({ error: "No such song" });
+
+  const idObj = { _id: id };
+  const bodyObj = { ...req.body };
+
+  const updatedSong = await song.updateSongById(idObj, bodyObj);
+  if (!updatedSong) return res.status(404).json("No such song");
+
+  res.status(200).json(updatedSong);
+};
+
+export { getSongs, createSong, getSong, deleteSong, updateSong };

3261ac0f335dc4ec8c7c2f52da7ca52cd31a765e
Michael Feleke <felekemichael999@gmail.com>
2024-06-18 14:20:18 +0300

    feat: Refactor songsController.js for improved code organization and readability

--- backend/models/songs/static.js
+++ backend/models/songs/static.js
@@ -2,10 +2,18 @@ export async function getAllSongs() {
   return this.find({}).sort({ createdAt: -1 });
 }
 
-export async function getOneSong(id) {
+export async function getSongById(id) {
   return this.findById(id);
 }
 
-export async function createOneSong(newSong) {
+export async function createNewSong(newSong) {
   return this.create(newSong);
+}
+
+export async function deleteSongById(id) {
+  return this.findOneAndDelete(id);
+}
+
+export async function updateSongById(idObj, bodyObj) {
+  return this.findOneAndUpdate(idObj, bodyObj);
 }

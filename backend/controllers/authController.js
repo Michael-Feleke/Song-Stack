c22c332a396719897120f8c59c58b04f0a7efbbb
Michael Feleke <felekemichael999@gmail.com>
2024-06-20 14:18:35 +0300

    feat: Update user authentication with token creation and logout functionality

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,8 +1,8 @@
 import { catchAsync } from "../utils/catchUtils.js";
 import user from "../models/users/usersModel.js";
-import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 import { MAX_TOKEN_AGE } from "../utils/constants.js";
+import { createToken } from "../utils/createToken.js";
 
 dotenv.config();
 
@@ -14,17 +14,26 @@ const userSignup = catchAsync(async (req, res) => {
   const token = createToken(createdUser._id);
   res.cookie("jwt", token, {
     httpOnly: true,
+    maxAge: MAX_TOKEN_AGE * 1000,
   });
-  res.status(200).json(createdUser);
+  res.status(200).json({ user: createdUser._id });
 });
 const userLogin = catchAsync(async (req, res) => {
-  res.json({ message: "user logged in" });
+  const { email, password } = req.body;
+  const loggedUser = await user.login(email, password);
+  const token = createToken(loggedUser._id);
+  res.cookie("jwt", token, {
+    httpOnly: true,
+    maxAge: MAX_TOKEN_AGE * 1000,
+  });
+  res.status(200).json({ user: loggedUser._id });
 });
 
-const createToken = (id) => {
-  return jwt.sign(id, process.env.SECRET_KEY, {
-    expiresIn: MAX_TOKEN_AGE,
+const userLogout = catchAsync(async (req, res) => {
+  res.cookie("jwt", "", {
+    maxAge: 1,
   });
-};
+  res.status(200).json({ message: "user logged out" });
+});
 
-export { userSignup, userLogin };
+export { userSignup, userLogin, userLogout };

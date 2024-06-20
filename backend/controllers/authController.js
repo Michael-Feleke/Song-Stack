87f958d0cc5df957b70692edf24901647420716a
Michael Feleke <felekemichael999@gmail.com>
2024-06-20 09:15:16 +0300

    feat: Add crypto and jsonwebtoken dependencies for user authentication

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,15 +1,30 @@
 import { catchAsync } from "../utils/catchUtils.js";
 import user from "../models/users/usersModel.js";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+import { MAX_TOKEN_AGE } from "../utils/constants.js";
+
+dotenv.config();
 
 const userSignup = catchAsync(async (req, res) => {
   const { email, password } = req.body;
   const newUser = { email, password };
 
   const createdUser = await user.createUser(newUser);
+  const token = createToken(createdUser._id);
+  res.cookie("jwt", token, {
+    httpOnly: true,
+  });
   res.status(200).json(createdUser);
 });
 const userLogin = catchAsync(async (req, res) => {
   res.json({ message: "user logged in" });
 });
+
+const createToken = (id) => {
+  return jwt.sign(id, process.env.SECRET_KEY, {
+    expiresIn: MAX_TOKEN_AGE,
+  });
+};
 
 export { userSignup, userLogin };

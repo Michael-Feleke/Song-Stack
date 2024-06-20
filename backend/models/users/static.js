7dff82ba4d84805ca050c2366d532dd968446908
Michael Feleke <felekemichael999@gmail.com>
2024-06-20 14:18:51 +0300

    feat: Add login functionality with password authentication

--- backend/models/users/static.js
+++ backend/models/users/static.js
@@ -1,7 +1,23 @@
+import bcrypt from "bcrypt";
+
 export async function createUser(newUser) {
   return this.create(newUser);
 }
 
 export async function findEmail(emailObj) {
   return this.findOne(emailObj);
+}
+
+export async function findUserById(id) {
+  return await this.findById(id);
+}
+
+export async function login(email, password) {
+  const user = await this.findOne({ email });
+  if (user) {
+    const auth = await bcrypt.compare(password, user.password);
+    if (auth) return user;
+    throw new Error("incorrect password");
+  }
+  throw new Error("The email is not registered");
 }

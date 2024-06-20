3f5dc8f84507244b32f59dbc5cc6649042e4f494
Michael Feleke <felekemichael999@gmail.com>
2024-06-20 08:38:09 +0300

    feat: Add user model and schema

---
+++ backend/models/users/usersSchema.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+import * as Statics from "./static.js";
+import isEmail from "validator/lib/isEmail.js";
+import bcrypt from "bcrypt";
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      validate: [isEmail, "Please enter the valid email"],
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+userSchema.static(Statics);
+
+userSchema.pre("save", async function (next) {
+  const salt = await bcrypt.genSalt();
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+export default userSchema;

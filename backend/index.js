0d50553079df18b9f47a710052e7354e13ca5b9c
Michael Feleke <felekemichael999@gmail.com>
2024-06-20 08:37:26 +0300

    chore: Add authentication route and cookie parsing

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,10 @@
 import dotenv from "dotenv";
 import express from "express";
+import authRoute from "./routes/authRoutes.js";
 import songsRoute from "./routes/songsRoute.js";
 import { connectDB } from "./config/database.js";
 import { globalErrorHandler } from "./controllers/errorController.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
@@ -20,10 +22,22 @@ app.use((req, res, next) => {
   next();
 });
 
+app.use("/auth", authRoute);
+
 app.use("/api/songs", songsRoute);
 
 app.use(globalErrorHandler);
 
 app.listen(PORT, () => {
-  console.log(`Listening to PORT ${PORT}`);
+  console.log(`Listening for requests on PORT ${PORT}`);
 });
+
+app.get("/set-cookies", (req, res) => {
+  res.cookie("newUser", false);
+  res.cookie("isLoggedIn", false, {
+    maxAge: 1000 * 60 * 60 * 24,
+    httpOnly: true,
+  });
+});
+
+app.get("/get-cookies", (req, res) => {});

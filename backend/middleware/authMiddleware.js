7278c16d90f47c53eed3e301c4293cdf716b13e4
Michael Feleke <felekemichael999@gmail.com>
2024-06-22 18:55:02 +0300

    feat: Remove checkUser middleware from authMiddleware

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -22,23 +22,4 @@ const requireAuth = (req, res, next) => {
   }
 };
 
-const checkUser = (req, res, next) => {
-  const token = req.cookies.jwt;
-  if (token) {
-    jwt.verify(token, process.env.SECRET_KEY, async (err, decodedToken) => {
-      if (err) {
-        console.log(err);
-        res.status(404).json({ message: "user not logged in" });
-      } else {
-        let foundUser = await user.findUserById(decodedToken.id);
-        req.foundUser = foundUser;
-        res.status(200).json(foundUser);
-      }
-    });
-  } else {
-    res.status(404).json({ message: "user not logged in" });
-    next();
-  }
-};
-
-export { requireAuth, checkUser };
+export { requireAuth };

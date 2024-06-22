43d061310c1b089fa99afe0a32903f00c9ffd141
Michael Feleke <felekemichael999@gmail.com>
2024-06-22 18:55:55 +0300

    feat: Add role field to userSchema

--- backend/models/users/usersSchema.js
+++ backend/models/users/usersSchema.js
@@ -19,6 +19,10 @@ const userSchema = new Schema(
       required: true,
       minlength: 6,
     },
+    role: {
+      type: String,
+      required: true,
+    },
   },
   {
     timestamps: true,
